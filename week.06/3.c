d7063507b92a2e5612d34269f892bf847466558c
sangkyu <doesol1301@gmail.com>
2023-10-15 00:47:42 +0900

    이진탐색 응용

---
+++ week.06/3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int left, right, cnt;
+  char answer;
+
+  scanf("%d %d %d", &left, &right, &cnt);
+  getchar();
+
+  for (int i = 0; i < cnt; i++){
+    int mid = (left + right) / 2;
+    scanf("%c", &answer);
+
+    if(answer == 'Y'){
+      left = mid + 1;
+    }
+    else if (answer == 'N') {
+      right = mid;
+    }
+  }
+  printf("%d", left);
+  return 0;
+}
